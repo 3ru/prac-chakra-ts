b40a217856715f1eea4bda5765daedda750d475f
Ryuyxx <Ryuyxx@gmail.com>
2021-06-25 15:07:43 +0900

    feat: login screen

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,5 +1,29 @@
 import React, { memo, VFC } from "react";
+import {
+	Box,
+	Flex,
+	Heading,
+	Input,
+	Button,
+	Divider,
+	Stack,
+} from "@chakra-ui/react";
 
 export const Login: VFC = memo(() => {
-	return <p>ログインページです</p>;
+	return (
+		<Flex align="center" justify="center" height="100vh">
+			<Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
+				<Heading as="h1" size="lg" textAlign="center">
+					ユーザー管理アプリ
+				</Heading>
+				<Divider my={4} />
+				<Stack spacing={6} py={4} px={10}>
+					<Input placeholder="User ID" />
+					<Button bg="teal.400" color="white" _hover={{ opacity: 0.6 }}>
+						Login
+					</Button>
+				</Stack>
+			</Box>
+		</Flex>
+	);
 });
